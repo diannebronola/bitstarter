2118769ceec293d0d9fd959db47b8999c63377ab
diannebronola <dbronola@gmail.com>
2013-07-18 07:01:52 +0000

    corrected syntax

--- web.js
+++ web.js
@@ -4,8 +4,9 @@ var buffer = new Buffer(fs.readFileSync("index.html"));
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('buffer.toString('utf-8'));
+app.get('/', function(request, response){
+ 
+response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
